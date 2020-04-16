d40e1f24e4a1c9771c14c44bb2251fc0c721f3c6
syu3ma4syo <syutvlove@gmail.com>
2020-04-16 22:36:38 +0900

    test add2

---
+++ webhook2.py
@@ -0,0 +1,16 @@
+from github_webhook import Webhook
+from flask import Flask
+
+app = Flask(__name__)  # Standard Flask app
+webhook = Webhook(app) # Defines '/postreceive' endpoint
+
+@app.route("/")        # Standard Flask endpoint
+def hello_world():
+    return "Hello, World!"
+
+@webhook.hook()        # Defines a handler for the 'push' event
+def on_push(data):
+    print("Got push with: {0}".format(data))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
